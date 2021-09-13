2197592a7b49dc673a1f40f280a69d62c6ed74dc
Adeeba Khan <i190626@nu.edu.pk>
2021-09-13 22:17:41 +0500

    Banking System Commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+package BMS;
+
+/* ___________________________________________________________________________________________*/
+/* -------------------------------------- MAIN CLASS ---------------------------------------- */
+public class Main {
+
+	public static void main(String[] args) {
+		BankManagementSystem OpenSystem = new BankManagementSystem();
+		OpenSystem.BankMenu();
+	}
+}
